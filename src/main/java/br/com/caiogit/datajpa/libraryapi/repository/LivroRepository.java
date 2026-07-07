7fa94f672753be3665cd20e3541d0f41259d6113
Caio Dias <caiodevgit@gmail.com>
2026-07-06 21:10:16 -0300

    Apliquei os Query methods no meu repository + adição de link direto para os testes. @Query utilizada para consultas personalizadas em uma linguagem diferente do sql padrão

--- src/main/java/br/com/caiogit/datajpa/libraryapi/repository/LivroRepository.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/repository/LivroRepository.java
@@ -3,13 +3,45 @@ package br.com.caiogit.datajpa.libraryapi.repository;
 import br.com.caiogit.datajpa.libraryapi.model.Autor;
 import br.com.caiogit.datajpa.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * @see br.com.caiogit.datajpa.libraryapi.repository.LivroRepositoryTest
+ */
 
-public interface LivroRepository extends JpaRepository<Livro, UUID>
-{
+
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
     // QUERY Method
     List<Livro> findByAutor(Autor autor);
+
+    List<Livro> findByTitulo(String titulo);
+
+    Livro findByIsbn(String isbn);
+
+    Livro findByAutorAndTitulo(Autor autor, String titulo);
+
+    List<Livro> findByPrecoGreaterThan(BigDecimal preco);
+
+    List<Livro> findByTituloContainingIgnoreCase(String titulo);
+
+
+    //Annotation que permite fazer a sua própria consulta personalizada
+    //JPQL -> referencia as entidades e as propriedades
+    @Query(" select l from Livro as l order by l.preco, l.titulo ")
+    List<Livro> listEveryoneByTituloAndPreco();
+
+    @Query(" select a from Livro l join l.autor a ")
+    List<Autor> findAutorOfLivros();
+
+    @Query("""
+            select l.genero
+            from Livro l
+            join l.autor a
+            where a.nacionalidade = 'Brasileira'
+            """)
+    List<String> listGeneroByAutor();
 }
