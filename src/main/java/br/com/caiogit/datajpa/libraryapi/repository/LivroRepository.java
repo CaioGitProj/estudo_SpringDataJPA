abd92247a573c7b3dff2a80eca4d4c29c4875e9f
Caio Dias <caiodevgit@gmail.com>
2026-07-08 21:48:13 -0300

    Escrevi métodos de update e delete de livros no banco de dados

--- src/main/java/br/com/caiogit/datajpa/libraryapi/repository/LivroRepository.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/repository/LivroRepository.java
@@ -4,10 +4,13 @@ import br.com.caiogit.datajpa.libraryapi.enums.GeneroLivro;
 import br.com.caiogit.datajpa.libraryapi.model.Autor;
 import br.com.caiogit.datajpa.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -51,4 +54,17 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
     //Usando os parâmetros dos métodos dentro das consultas
     @Query(" select l from Livro l where l.generoLivro = :genero order by :paramOrdenacao")
     List<Livro> findByGenero(@Param("genero") GeneroLivro genero, @Param("paramOrdenacao") String nomePropriedade);
+
+
+    //Delete usando parâmetro posicional
+    @Modifying //Eu preciso dessa anotação, pois estou modificando o banco
+    @Transactional //Garante que a operação seja executada numa transação segura
+    @Query(" delete from Livro where generoLivro = ?1 ")
+    void deleteByGeneroLivro(GeneroLivro genero);
+
+
+    @Modifying
+    @Transactional
+    @Query(" update Livro l set l.dataPublicacao = :novaData where l.dataPublicacao < :dataUpdate ")
+    void updateDataPublicacao(@Param("novaData") LocalDate novaData, @Param("dataUpdate") LocalDate dataUpdate);
 }
