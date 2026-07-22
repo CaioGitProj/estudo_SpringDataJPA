e6834595d1ca12cfa3cfcdd533474f1c4eb60bb2
Caio Dias <caiodevgit@gmail.com>
2026-07-22 20:47:52 -0300

    novos campos para o dto(facilitando operações futuras que vão usar o Id)

--- src/main/java/br/com/caiogit/datajpa/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/controller/dto/AutorDTO.java
@@ -4,8 +4,10 @@ package br.com.caiogit.datajpa.libraryapi.controller.dto;
 import br.com.caiogit.datajpa.libraryapi.model.Autor;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public record AutorDTO(
+        UUID id,
         String nome,
         LocalDate dataNascimento,
         String nacionalidade)
