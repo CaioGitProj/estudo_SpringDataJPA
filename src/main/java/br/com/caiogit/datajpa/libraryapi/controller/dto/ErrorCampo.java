a1563efbc285930e86f48de7d5126c4496745f74
Caio Dias <caiodevgit@gmail.com>
2026-07-23 12:45:15 -0300

    dtos para padronização de erros para facilitar o entendimento da api

---
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/controller/dto/ErrorCampo.java
@@ -0,0 +1,6 @@
+package br.com.caiogit.datajpa.libraryapi.controller.dto;
+
+public record ErrorCampo(String campo, String erro)
+{
+
+}
---
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/controller/dto/ErrorResposta.java
@@ -0,0 +1,19 @@
+package br.com.caiogit.datajpa.libraryapi.controller.dto;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public record ErrorResposta(int status, String mensagem, List<ErrorCampo> errorCampoList)
+{
+    public static ErrorResposta respostaPadrao(String mensagem)
+    {
+        return new ErrorResposta(HttpStatus.BAD_REQUEST.value(), mensagem, List.of());
+    }
+
+    public static ErrorResposta conflito(String mensagem)
+    {
+        return new ErrorResposta(HttpStatus.CONFLICT.value(), mensagem, List.of());
+    }
+
+}
