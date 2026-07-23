fbc7f5056f0c4a4dfc97ff93d90c361b2bedc39c
Caio Dias <caiodevgit@gmail.com>
2026-07-22 21:58:24 -0300

    operações de deleção, criação e pesquisa parametrizada pelo nome e nacionalidade do Autor(caso seja especificado)

--- src/main/java/br/com/caiogit/datajpa/libraryapi/controller/AutorController.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/controller/AutorController.java
@@ -3,15 +3,14 @@ package br.com.caiogit.datajpa.libraryapi.controller;
 import br.com.caiogit.datajpa.libraryapi.controller.dto.AutorDTO;
 import br.com.caiogit.datajpa.libraryapi.model.Autor;
 import br.com.caiogit.datajpa.libraryapi.service.AutorService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/autores") //http://localhst:8080/autores
@@ -35,10 +34,92 @@ public class AutorController
         //Caso ele consiga salvar no banco, retorna na url o id do autor criado
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
-                .path("{id}")
+                .path("/{id}")
                 .buildAndExpand(autor.getId())
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AutorDTO> obterDetalhesAutor(@PathVariable("id") String id)
+    {
+
+        try
+        {
+            var idAutor = UUID.fromString(id);
+            Optional<Autor> autorOptional = autorService.obterAutorPorId(idAutor);
+
+            if(autorOptional.isPresent())
+            {
+                Autor entidade = autorOptional.get();
+
+                AutorDTO dto = new AutorDTO(
+                        entidade.getId(),
+                        entidade.getNome(),
+                        entidade.getDataNascimento(),
+                        entidade.getNacionalidade());
+
+                return ResponseEntity.ok(dto);
+            }
+        }
+        catch (IllegalArgumentException e)
+        {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarAutor(@PathVariable("id") String id)
+    {
+
+        try
+        {
+            var idAutor = UUID.fromString(id);
+            if(autorService.obterAutorPorId(idAutor).isPresent())
+            {
+                Optional<Autor> autorOptional = autorService.obterAutorPorId(idAutor);
+
+                autorService.deletarAutor(autorOptional.get());
+            }
+            else
+            {
+                return ResponseEntity.notFound().build();
+            }
+
+        }
+        catch(IllegalArgumentException e)
+        {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<AutorDTO>> pesquisarAutores(
+            @RequestParam(value ="nome", required = false) String nome,
+            @RequestParam(value ="nacionalidade", required = false) String nacionalidade)
+    {
+
+        List<Autor> resultadoAutor = autorService.pesquisarAutor(nome, nacionalidade);
+
+        List<AutorDTO> listaRetorno = resultadoAutor
+                .stream()
+                .map(
+                        (autor) -> new AutorDTO(autor.getId(),
+                                autor.getNome(),
+                                autor.getDataNascimento(),
+                                autor.getNacionalidade())
+                )
+                .toList();
+
+
+        return ResponseEntity.ok(listaRetorno);
     }
 }
--- src/main/java/br/com/caiogit/datajpa/libraryapi/service/AutorService.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/service/AutorService.java
@@ -4,6 +4,10 @@ import br.com.caiogit.datajpa.libraryapi.model.Autor;
 import br.com.caiogit.datajpa.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class AutorService
 {
@@ -19,5 +23,33 @@ public class AutorService
     public Autor salvarAutor(Autor autor)
     {
         return autorRepository.save(autor);
+    }
+
+    public Optional<Autor> obterAutorPorId(UUID id)
+    {
+        return autorRepository.findById(id);
+    }
+
+    public void deletarAutor(Autor autor)
+    {
+        autorRepository.delete(autor);
+    }
+
+    public List<Autor> pesquisarAutor(String nome, String nacionalidade)
+    {
+        if(nome != null && nacionalidade != null) {
+            return autorRepository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+
+        else if(nome != null) {
+            return autorRepository.findByNome(nome);
+        }
+
+        else if(nacionalidade != null) {
+            return autorRepository.findByNacionalidade(nacionalidade);
+        }
+
+        return autorRepository.findAll();
+
     }
 }
