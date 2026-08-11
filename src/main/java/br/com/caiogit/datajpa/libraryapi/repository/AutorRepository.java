a8b242e4fb23e14ab8de7bbddc8e86edb1b098c2
Caio Dias <caiodevgit@gmail.com>
2026-08-10 21:34:47 -0300

    Criei uma nova operação personalizada para encontrar um Autor que possua o mesmo nome, data de nascimento e nacionalidade. Essa operação conversa com o validador de registro duplicado e a nova exceção personalizada para a classe de Autor

--- src/main/java/br/com/caiogit/datajpa/libraryapi/repository/AutorRepository.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/repository/AutorRepository.java
@@ -3,8 +3,21 @@ package br.com.caiogit.datajpa.libraryapi.repository;
 import br.com.caiogit.datajpa.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 
-public interface AutorRepository extends JpaRepository<Autor, UUID> {
+public interface AutorRepository extends JpaRepository<Autor, UUID>
+{
+
+    List<Autor> findByNome(String nome);
+    List<Autor> findByNacionalidade(String nacionalidade);
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
+
+
+    Optional<Autor> findByNomeAndDataNascimentoAndNacionalidade(
+            String nome, LocalDate dataNascimento, String nacionalidade);
+
 }
