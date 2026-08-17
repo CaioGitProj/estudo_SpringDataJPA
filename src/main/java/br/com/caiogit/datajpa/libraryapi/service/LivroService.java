8cbd15ed569c6d106ebc245ab8c8c487577e4ef5
Caio Dias <caiodevgit@gmail.com>
2026-08-17 20:12:27 -0300

    Criei um novo service para a classe Livro

--- src/main/java/br/com/caiogit/datajpa/libraryapi/service/LivroService.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/service/LivroService.java
@@ -1,4 +1,16 @@
 package br.com.caiogit.datajpa.libraryapi.service;
 
-public class LivroService {
+
+import br.com.caiogit.datajpa.libraryapi.repository.LivroRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class LivroService
+{
+    private final LivroRepository livrorepository;
+
+
 }
