ac58098967eb68764d0cde8f55fdeca6cc0089fd
Caio Dias <caiodevgit@gmail.com>
2026-08-19 21:55:06 -0300

    Criei uma interface contendo operações genéricas de controllers, evitando boilerplate

--- src/main/java/br/com/caiogit/datajpa/libraryapi/controller/GenericController.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/controller/GenericController.java
@@ -1,4 +1,20 @@
 package br.com.caiogit.datajpa.libraryapi.controller;
 
-public interface GenericController {
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+public interface GenericController
+{
+    // Cria uma header com uma URL desse tipo: http://caminho:porta/Object?/{id}
+    //exemplo : http://localhst:8080/autores/{id}
+    default URI gerarHeaderLocation(UUID id)
+    {
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}").buildAndExpand(id)
+                .toUri();
+    }
+
 }
