31bf8e03fc20da1d4b03b3906c2be6c71eafd3f5
Caio Dias <caiodevgit@gmail.com>
2026-08-19 21:56:09 -0300

    Criei novas classes mappers(Uma para autor, outra para livro)

--- src/main/java/br/com/caiogit/datajpa/libraryapi/controller/dto/mappers/AutorMapper.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/controller/dto/mappers/AutorMapper.java
@@ -1,4 +1,13 @@
 package br.com.caiogit.datajpa.libraryapi.controller.dto.mappers;
 
-public interface AutorMapper {
+import br.com.caiogit.datajpa.libraryapi.controller.dto.AutorDTO;
+import br.com.caiogit.datajpa.libraryapi.model.Autor;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface AutorMapper
+{
+    Autor toEntity(AutorDTO dto); //transforma obj AutorDTO em Autor
+
+    AutorDTO toDTO(Autor autor); //transforma obj Autor em AutorDTO
 }
--- src/main/java/br/com/caiogit/datajpa/libraryapi/controller/dto/mappers/LivroMapper.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/controller/dto/mappers/LivroMapper.java
@@ -1,4 +1,19 @@
 package br.com.caiogit.datajpa.libraryapi.controller.dto.mappers;
 
-public interface LivroMapper {
+import br.com.caiogit.datajpa.libraryapi.controller.dto.CadastroLivroDTO;
+import br.com.caiogit.datajpa.libraryapi.model.Livro;
+import br.com.caiogit.datajpa.libraryapi.repository.AutorRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring")
+public abstract class LivroMapper
+{
+    @Autowired
+    AutorRepository autorRepository;
+
+    //Como Livro precisa de um objeto do tipo autor, tem que acessar o banco de dados(autor tem que existir)
+    @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null) )")
+    public abstract Livro toEntity(CadastroLivroDTO dto);
 }
