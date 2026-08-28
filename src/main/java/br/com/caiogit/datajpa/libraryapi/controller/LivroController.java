17b1a03213cf7f121fad9c7f470c5c6c88496a07
Caio Dias <caiodevgit@gmail.com>
2026-08-28 11:27:59 -0300

    Novas operações de pesquisa de livros: uma usando specifications, outra é uma busca comum do repository. Além, uma operação básica de deleção de livros

--- src/main/java/br/com/caiogit/datajpa/libraryapi/controller/LivroController.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/controller/LivroController.java
@@ -1,18 +1,19 @@
 package br.com.caiogit.datajpa.libraryapi.controller;
 
 import br.com.caiogit.datajpa.libraryapi.controller.dto.CadastroLivroDTO;
-import br.com.caiogit.datajpa.libraryapi.controller.dto.error.ErrorResposta;
+import br.com.caiogit.datajpa.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import br.com.caiogit.datajpa.libraryapi.controller.dto.mappers.LivroMapper;
-import br.com.caiogit.datajpa.libraryapi.exceptions.RegistroDuplicadoException;
+import br.com.caiogit.datajpa.libraryapi.enums.GeneroLivro;
 import br.com.caiogit.datajpa.libraryapi.model.Livro;
 import br.com.caiogit.datajpa.libraryapi.service.LivroService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("livros")
@@ -30,5 +31,43 @@ public class LivroController implements GenericController {
 
         var url = gerarHeaderLocation(livro.getId());
         return ResponseEntity.created(url).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResultadoPesquisaLivroDTO> obterDetalhesLivro(@PathVariable("id") String id)
+    {
+        return livroService
+                .obterLivroPorId(UUID.fromString(id))
+                .map(livro -> {
+            var dto = mapper.toDTO(livro);
+            return ResponseEntity.ok(dto);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletarLivro( @PathVariable("id") String id)
+    {
+        return livroService.obterLivroPorId(UUID.fromString(id))
+                .map(livro -> {
+            livroService.deletarLivro(livro);
+            return ResponseEntity.noContent().build();
+        })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResultadoPesquisaLivroDTO>> pesquisaLivroSpec(
+            @RequestParam(value = "isbn", required = false) String isbn,
+            @RequestParam(value = "titulo", required = false) String titulo,
+            @RequestParam(value = "nome-autor",required = false) String nomeAutor,
+            @RequestParam(value = "genero", required = false)GeneroLivro genero,
+            @RequestParam(value = "ano-publicacao", required = false) Integer anoPublicacao
+            )
+    {
+        var resultadoPesquisa = livroService.pesquisarLivro(isbn, titulo,nomeAutor,genero,anoPublicacao);
+
+        var lista = resultadoPesquisa.stream().map(mapper::toDTO).toList();
+
+        return ResponseEntity.ok(lista);
     }
 }
--- src/main/java/br/com/caiogit/datajpa/libraryapi/service/LivroService.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/service/LivroService.java
@@ -1,10 +1,19 @@
 package br.com.caiogit.datajpa.libraryapi.service;
 
 
+import br.com.caiogit.datajpa.libraryapi.enums.GeneroLivro;
+import br.com.caiogit.datajpa.libraryapi.model.Livro;
 import br.com.caiogit.datajpa.libraryapi.repository.LivroRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
+import static br.com.caiogit.datajpa.libraryapi.repository.specs.LivroSpecs.*;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +21,44 @@ public class LivroService
 {
     private final LivroRepository livrorepository;
 
+    public void salvarLivro(Livro livro)
+    {
+        livrorepository.save(livro);
+    }
 
+    public Optional<Livro> obterLivroPorId(UUID id)
+    {
+        return livrorepository.findById(id);
+    }
+
+    public void deletarLivro(Livro livro)
+    {
+        livrorepository.delete(livro);
+    }
+
+    public List<Livro> pesquisarLivro(String isbn, String titulo, String nomeAutor, GeneroLivro genero, Integer anoPublicacao)
+    {
+        // select * from livro where isbn = :isbn and nomeAutor = :nomeAutor
+        // conjunction é um where que sempre vai dar positivo( 0 = 0)
+        Specification<Livro> specs = Specification.where((root, query, cb) -> cb.conjunction());
+
+        if(isbn != null)
+        {
+            specs = specs.and(isbnEqual(isbn));
+        }
+        if(titulo != null)
+        {
+            specs = specs.and(tituloLike(titulo));
+        }
+        if(genero != null)
+        {
+            specs = specs.and(generoEqual(genero));
+        }
+        if(anoPublicacao != null)
+        {
+            specs = specs.and(anoPublicacaoEqual(anoPublicacao));
+        }
+
+        return livrorepository.findAll(specs);
+    }
 }
