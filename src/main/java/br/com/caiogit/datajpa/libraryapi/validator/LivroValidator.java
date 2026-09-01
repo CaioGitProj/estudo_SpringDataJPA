78dd1011780328007f7be148ef2ab83129f00fc9
Caio Dias <caiodevgit@gmail.com>
2026-08-31 21:55:22 -0300

    Criei um validador de livros que retorna RegistroDuplicadoException ou CampoInvalidoException caso o isbn do livro já exista e o usuário estiver tentando cadastrar um novo, ou se o livro foi publicado em 2020+ e o preço está nulo

--- src/main/java/br/com/caiogit/datajpa/libraryapi/validator/LivroValidator.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/validator/LivroValidator.java
@@ -1,4 +1,50 @@
 package br.com.caiogit.datajpa.libraryapi.validator;
 
-public class LivroValidator {
+import br.com.caiogit.datajpa.libraryapi.exceptions.CampoInvalidoException;
+import br.com.caiogit.datajpa.libraryapi.exceptions.RegistroDuplicadoException;
+import br.com.caiogit.datajpa.libraryapi.model.Livro;
+import br.com.caiogit.datajpa.libraryapi.repository.LivroRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class LivroValidator
+{
+    private static final int ANO_EXIGENCIA_PRECO = 22;
+
+    private final LivroRepository livroRepository;
+
+    public void validarLivro(Livro livro)
+    {
+        if(existeLivroComIsbn(livro))
+        {
+            throw new RegistroDuplicadoException("ISBN já cadastrado");
+        }
+
+        if(isPrecoObrigatorioNulo(livro))
+        {
+            throw new CampoInvalidoException("preco", "Para livros com ano de publicação a partir de 2020, o preço é obrigatório");
+        }
+    }
+
+    private boolean isPrecoObrigatorioNulo(Livro livro)
+    {
+        return livro.getPreco() == null
+                && livro.getDataPublicacao().getYear() >= ANO_EXIGENCIA_PRECO;
+    }
+
+    private boolean existeLivroComIsbn(Livro livro)
+    {
+        Optional<Livro> livroEncontrado = livroRepository.findByIsbn(livro.getIsbn());
+
+        if(livro.getId() == null)
+        {
+            return livroEncontrado.isPresent();
+        }
+
+        return livroEncontrado.map(Livro::getId).stream().anyMatch(id -> !id.equals(livro.getId()));
+    }
 }
