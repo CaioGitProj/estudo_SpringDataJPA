718d0b9dbd12337b2b5d5d763df39b5a6caa1b09
Caio Dias <caiodevgit@gmail.com>
2026-09-01 22:49:10 -0300

    Pasta contendo controllers MVC para rodar páginas web

--- src/main/java/br/com/caiogit/datajpa/libraryapi/controller/pageController/LoginViewController.java
+++ src/main/java/br/com/caiogit/datajpa/libraryapi/controller/pageController/LoginViewController.java
@@ -1,4 +1,4 @@
-package br.com.caiogit.datajpa.libraryapi.controller.PageController;
+package br.com.caiogit.datajpa.libraryapi.controller.pageController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
